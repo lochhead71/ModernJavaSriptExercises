dafbfc4a560eac2a9e8301b178eefd9cf90c66b2
lochhead71 <lochhead71@gmail.com>
2021-08-02 09:40:27 -0500

    General Loops

--- app-Sec.2.17-21.js
+++ app-Sec.2.17-21.js
@@ -47,44 +47,128 @@
 
  // FUNCTION DECLARATIONS & EXPRESSIONS 2.18
 
- function greet(firstName = 'Rusty', lastName = 'Speedracer'){
-   //  console.log('Hello');
-   return 'hello ' + firstName + ' ' + lastName;
- }
+//  function greet(firstName = 'Rusty', lastName = 'Speedracer'){
+//    //  console.log('Hello');
+//    return 'hello ' + firstName + ' ' + lastName;
+//  }
 
-//  console.log(greet());
+// //  console.log(greet());
 
-const square = function(x){
-   return x*x;
-};
+// const square = function(x){
+//    return x*x;
+// };
 
-// console.log(5);
+// // console.log(5);
 
-// Immediately Invokable Function Expressions (IIFEs)
+// // Immediately Invokable Function Expressions (IIFEs)
 
-// (function(){
-//    console.log('IIFEs Rule!');
-// })();
+// // (function(){
+// //    console.log('IIFEs Rule!');
+// // })();
 
-// (function(newName){
-//    console.log('What\'s happening ' + newName + '?');
-// })();
+// // (function(newName){
+// //    console.log('What\'s happening ' + newName + '?');
+// // })();
 
-// Property Methods
+// // Property Methods
 
-const todo = {
-   add: function(){
-      console.log('Added an Item');
-   },
-   edit: function(id){
-      console.log(`Edit item ${id}`);
-   }
+// const todo = {
+//    add: function(){
+//       console.log('Added an Item');
+//    },
+//    edit: function(id){
+//       console.log(`Edit item ${id}`);
+//    }
+// }
+
+// todo.delete = function(){
+//    console.log('todo deleted.');
+// }
+
+// todo.add();
+// todo.edit(25);
+// todo.delete();
+
+// FOR LOOPS
+
+// for(let i = 0; i < 10; i++){
+//    if(i === 5){
+//       console.log('The magic number is ' + i)
+//    } else {
+//    console.log('The number is ' + i);
+//    }
+// }
+// for(let i = 0; i <= 10; i++){
+//    if(i === 5){
+//       console.log('The magic number is ' + i);
+//    continue;
+//    }
+//    console.log('The number is ' + i);
+// }
+
+// for(let i = 0; i <= 10; i++){
+//    if(i === 5){
+//       console.log('The magic number is ' + i);
+//    break;
+//    }
+//    console.log('The number is ' + i);
+// }
+
+
+// WHILE + DO WHILE LOOPS (Do While will always run once regardless of condition)
+// let i = 0;
+// while(i < 10){
+//    console.log('Number ' + i);
+//    i++;
+// }
+
+// let i = 100;
+
+// do {
+//    console.log('Number ' + i);
+//    i++;
+// }
+
+// while(i < 10);
+
+//LOOP THROUGH AN ARRAY
+
+const cars = ['sedan', 'hatchback', 'coupe', 'station wagon', 'cross-over'];
+
+// for(i = 0; i < cars.length; i++){
+//    console.log(cars[i]);
+// }
+
+// cars.forEach(function(car, index, array){
+//    console.log(`${index} : ${car}`);
+//    console.log(array);
+// });
+
+//MAPPING AN ARRAY
+
+// const users = [
+//    {id: 1, name: 'Bob'},
+//    {id: 2, name: 'Jerry'},
+//    {id: 3, name: 'Phil'},
+//    {id: 4, name: 'Pigpen'},
+//    {id: 5, name: 'Mickey'}
+// ];
+
+// const ids = users.map(function(user){
+//    return user.id;
+// });
+
+// console.log(ids);
+
+//FOR IN LOOP
+
+const user = {
+   firstName: 'Jerry',
+   lastName: 'Garcia',
+   instrument: 'guitar',
+   status: 'God'
 }
 
-todo.delete = function(){
-   console.log('todo deleted.');
+for(let x in user){
+   console.log(`${x} : ${user[x]}`);
 }
-
-todo.add();
-todo.edit(25);
-todo.delete();
