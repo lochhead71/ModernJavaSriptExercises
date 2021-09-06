92c1e1c5402bd1ac503e0ca596472012b4fe1cf5
lochhead71 <lochhead71@gmail.com>
2021-09-06 13:06:15 -0500

    Got it all working.

---
+++ Section 7/7_4_project_files/js_sandbox/app.js
@@ -0,0 +1,46 @@
+// Create Event Listener for Button 1
+document.getElementById('button1').addEventListener('click', loadCustomer);
+function loadCustomer(e){
+   const xhr = new XMLHttpRequest();
+   xhr.open('GET', 'customer.json', true);
+   xhr.onload = function(){
+      if(this.status === 200){
+         const customer = JSON.parse(this.responseText);
+         const output = `
+            <ul>
+               <li>ID: ${customer.id}</li>
+               <li>Name: ${customer.name}</li>
+               <li>Company: ${customer.company}</li>
+               <li>Phone: ${customer.phone}</li>
+            </ul>
+         `;
+         document.getElementById('customer').innerHTML = output;
+      }
+   }
+   xhr.send();
+}
+
+// Create Event Listener for Button 2
+document.getElementById('button2').addEventListener('click', loadCustomers);
+function loadCustomers(e){
+   const xhr = new XMLHttpRequest();
+   xhr.open('GET', 'customers.json', true);
+   xhr.onload = function(){
+      if(this.status === 200){
+         const customers = JSON.parse(this.responseText);
+         let output = '';
+         customers.forEach(function(customer){
+            output += `
+               <ul>
+                  <li>ID: ${customer.id}</li>
+                  <li>Name: ${customer.name}</li>
+                  <li>Company: ${customer.company}</li>
+                  <li>Phone: ${customer.phone}</li>
+               </ul>
+            `;
+         });
+         document.getElementById('customers').innerHTML = output;
+      }
+   }
+   xhr.send();
+}
