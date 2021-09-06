24cceda8e755b78000c2533eaf07bbe038eb7b53
lochhead71 <lochhead71@gmail.com>
2021-09-06 09:11:19 -0500

    Created app using ES5 objects

---
+++ Section 6/appES6.js
@@ -0,0 +1,12 @@
+// ES6 Class for Book
+class Book {
+   constructor(title, author, isbn){
+      this.title = title;
+      this.author = author;
+      this.isbn = isbn;
+   }
+}
+
+
+
+// ES6 Class for UI
