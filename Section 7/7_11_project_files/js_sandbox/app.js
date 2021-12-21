e3b76886ddf75b95d2d4a5efb911419fff8ebebb
lochhead71 <lochhead71@gmail.com>
2021-12-21 15:12:10 -0600

    back on track

--- Section 7/7_11_project_files/js_sandbox/app.js
+++ Section 7/7_11_project_files/js_sandbox/app.js
@@ -1,9 +1,33 @@
-document.getElementById('button1').addEventListener('click', getText);
+document.getElementById("button1").addEventListener("click", getText);
+
+document.getElementById("button2").addEventListener("click", getJson);
 
 function getText() {
-   fetch('test.txt')
-   .then(function(res){
-      console.log(res);
-   });
-   console.log('Testing');
+  fetch("test.txt")
+    .then(function (res) {
+      return res.text();
+    })
+    .then(function (data) {
+      console.log(data);
+      document.getElementById("output").innerHTML = data;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+function getJson() {
+  fetch("posts.json")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      let output = "";
+      data.forEach(function (post) {
+        output += "<li>${post.title}</li>";
+      });
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
