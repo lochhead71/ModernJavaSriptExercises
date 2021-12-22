266afe85dd835526b69e80830303f33cac8b41b8
lochhead71 <lochhead71@gmail.com>
2021-12-22 07:36:31 -0600

    Doin' things

---
+++ Section 7/7_13_project_files/easyhttp2/easyhttp2.js
@@ -0,0 +1,22 @@
+/**
+ * EasyHTTP Library
+ * Library for making HTTP requests
+ *
+ * @version 2.0.0
+ * @author  Brad Traversy
+ * @license MIT
+ *
+ **/
+
+class EasyHTTP {
+	// Make an HTTP GET Request
+	get(url) {
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => console.log(data))
+			.catch((err) => console.log(err));
+	}
+	// Make an HTTP PUT Request
+
+	// Make an HTTP PUT Request
+}
