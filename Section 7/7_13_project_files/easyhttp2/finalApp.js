adf537704854d6f714e6f1dedc4802441ba46f29
lochhead71 <lochhead71@gmail.com>
2021-12-22 12:41:41 -0600

    Modern JS http framework

--- Section 7/7_13_project_files/easyhttp2/finalApp.js
+++ Section 7/7_13_project_files/easyhttp2/finalApp.js
@@ -1,16 +1,17 @@
-const http = new EasyHTTP;
+const http = new EasyHTTP();
 
 // Get Users
-// http.get('https://jsonplaceholder.typicode.com/users')
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
+http
+	.get('https://jsonplaceholder.typicode.com/users')
+	.then((data) => console.log(data))
+	.catch((err) => console.log(err));
 
 // User Data
-const data = {
-  name: 'John Doe',
-  username: 'johndoe',
-  email: 'jdoe@gmail.com'
-}
+// const data = {
+//   name: 'John Doe',
+//   username: 'johndoe',
+//   email: 'jdoe@gmail.com'
+// }
 
 // Create User
 // http.post('https://jsonplaceholder.typicode.com/users', data)
@@ -23,6 +24,6 @@ const data = {
 //   .catch(err => console.log(err));
 
 // Delete User
-http.delete('https://jsonplaceholder.typicode.com/users/2')
-.then(data => console.log(data))
-.catch(err => console.log(err));
+// http.delete('https://jsonplaceholder.typicode.com/users/2')
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
