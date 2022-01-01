3601b32256ef5b3647e5c039dbaa77792d7f3d0b
lochhead71 <lochhead71@gmail.com>
2022-01-01 13:37:23 -0600

    Yay... sets!

--- Section 10/10_6_project_files/js_sandbox/app.js
+++ Section 10/10_6_project_files/js_sandbox/app.js
@@ -1,44 +1,32 @@
-// SETS - Store unique values of any type
-
+// SETS - store unique values of any type
 const set1 = new Set();
-
 // Add values to set
 set1.add(100);
-set1.add('A string');
-set1.add({name: 'John'});
-set1.add(true);
-set1.add(100);
+set1.add('some string');
+set1.add({ name: 'Jerry Garcia' });
+set1.add(false);
+// console.log(set1);
 
-// const set2 = new Set([1, true, 'string']);
+const set2 = new Set([100, 'another string', { key: 'value' }, true]);
 // console.log(set2);
+// console.log(set2.size);
 
+// Check for values - no objects
+// console.log(set1.has('some string'), set2.has(25 * 4));
+
+// Delete from set - no objects
+// set1.delete('some string');
 // console.log(set1);
 
-// Get count
-// console.log(set1.size);
-
-// Check for values
-// console.log(set1.has(100));
-// console.log(set1.has(50 + 50));
-// console.log(set1.has({name: 'John'}));
-
-// Delete from set
-// set1.delete(100);
-
-// console.log(set1);
-
-// ITERATING THROUGH SETS
-
-// For..of 
-// for(let item of set1) {
-//   console.log(item);
+// Iterate through sets - for...of or forEach
+// for (let item of set1) {
+// 	console.log(item);
 // }
 
-// ForEach Loop
 // set1.forEach((value) => {
-//   console.log(value);
+// 	console.log(value);
 // });
 
-// CONVERT SET TO ARRAY
-const setArr = Array.from(set1);
+// Convert sets to arrays
+const setArr = Array.from(set2);
 console.log(setArr);
