f97f037bdd34391ae25932ed2fbaadfa84e12d82
lochhead71 <lochhead71@gmail.com>
2022-01-02 10:37:26 -0600

    Not sure if singletons will be useful

--- Section 11/11_3_project_files/js_sandbox/app.js
+++ Section 11/11_3_project_files/js_sandbox/app.js
@@ -1,24 +1,22 @@
-const Singleton = (function() {
-  let instance;
+// SINGLETON PATTERN - you can only return one instance
+const Singleton = (function () {
+	let instance;
 
-  function createInstance() {
-    const object = new Object({name:'Brad'});
-    return object;
-  }
+	function createInstance() {
+		const object = new Object({ lyric: 'If I knew the way...' });
+		return object;
+	}
 
-  return {
-    getInstance: function() {
-      if(!instance){
-        instance = createInstance();
-      }
-      return instance;
-    }
-  }
+	return {
+		getInstance: function () {
+			if (!instance) {
+				instance = createInstance();
+			}
+			return instance;
+		},
+	};
 })();
 
 const instanceA = Singleton.getInstance();
 const instanceB = Singleton.getInstance();
-
 console.log(instanceA === instanceB);
-
-// console.log(instanceA);
