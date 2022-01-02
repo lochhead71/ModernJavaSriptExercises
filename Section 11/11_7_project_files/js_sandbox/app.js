e40e2f5215b7131b8e7959811c4b414f23b0a9fa
lochhead71 <lochhead71@gmail.com>
2022-01-02 17:09:07 -0600

    State pattern - just like Redux!

--- Section 11/11_7_project_files/js_sandbox/app.js
+++ Section 11/11_7_project_files/js_sandbox/app.js
@@ -1,85 +1,84 @@
-const PageState = function() {
-  let currentState = new homeState(this);
+const pageState = function () {
+	let currentState = new homeState(this);
 
-  this.init = function() {
-    this.change(new homeState);
-  }
+	this.init = function () {
+		this.change(new homeState());
+	};
 
-  this.change = function(state) {
-    currentState = state;
-  }
+	this.change = function (state) {
+		currentState = state;
+	};
 };
 
 // Home State
-const homeState = function(page) {
-  document.querySelector('#heading').textContent = null;
-  document.querySelector('#content').innerHTML = `
-    <div class="jumbotron">
-    <h1 class="display-3">Hello, world!</h1>
-    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-    <hr class="my-4">
-    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-    <p class="lead">
-      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-    </p>
-  </div>
-  `;
+
+const homeState = function (page) {
+	document.querySelector('#heading').textContent = null;
+	document.querySelector('#content').innerHTML = `
+      <div class="jumbotron">
+         <h1 class="display-3">Hello, world!</h1>
+         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+         <hr class="my-4">
+         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+         <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+         </p>
+      </div>
+   `;
 };
 
-// About State
-const aboutState = function(page) {
-  document.querySelector('#heading').textContent = 'About Us';
-  document.querySelector('#content').innerHTML = `
-    <p>This is the about page</p>
-`;
+//  About State
+const aboutState = function (page) {
+	document.querySelector('#heading').textContent = 'About Us';
+	document.querySelector('#content').innerHTML = `
+      <p>This is the about page</p>
+   `;
 };
 
-// Contact State
-const contactState = function(page) {
-  document.querySelector('#heading').textContent = 'Contact Us';
-  document.querySelector('#content').innerHTML = `
-  <form>
-    <div class="form-group">
-      <label>Name</label>
-      <input type="text" class="form-control">
-    </div>
-    <div class="form-group">
-    <label>Email address</label>
-    <input type="email" class="form-control">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-`;
+//  Contact State
+const contactState = function (page) {
+	document.querySelector('#heading').textContent = 'Contact Us';
+	document.querySelector('#content').innerHTML = `
+      <form>
+         <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control">
+         </div>
+         <div class="form-group">
+         <label>Email address</label>
+         <input type="email" class="form-control">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+   `;
 };
 
 // Instantiate pageState
-const page = new PageState();
 
-// Init the first state
+const page = new pageState();
+
+// Initiatlize the first state
 page.init();
 
-// UI Vars
+// UI variables
 const home = document.getElementById('home'),
-      about = document.getElementById('about'),
-      contact = document.getElementById('contact');
+	about = document.getElementById('about'),
+	contact = document.getElementById('contact');
 
-// Home
+// Home Event Listener + Click Handler
 home.addEventListener('click', (e) => {
-  page.change(new homeState);
-
-  e.preventDefault();
+	page.change(new homeState());
+	e.preventDefault();
 });
 
-// About
+// About Event Listener + Click Handler
 about.addEventListener('click', (e) => {
-  page.change(new aboutState);
-
-  e.preventDefault();
+	page.change(new aboutState());
+	e.preventDefault();
 });
 
-// Contact
+// Contact Event Listener + Click Handler
 contact.addEventListener('click', (e) => {
-  page.change(new contactState);
-
-  e.preventDefault();
+	page.change(new contactState());
+	e.preventDefault();
 });
